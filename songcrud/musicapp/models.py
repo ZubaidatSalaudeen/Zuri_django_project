bd0dc0183ba8c9ba40766b239aa5b737021f48f3
Zubaidat <zubaidatsalaudeen@gmail.com>
2022-10-18 18:41:51 +0100

    django CRUD

--- songcrud/musicapp/models.py
+++ songcrud/musicapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Artiste(models.Model):
+    first_name = models.CharField(max_length = 50)
+    last_name = models.CharField(max_length = 50)
+    age = models.IntegerField()
+class Song(models.Model):
+    title = models.CharField(max_length = 50)
+    date_released = models.DateField()
+    likes = models.IntegerField()
+    artiste_id = models.ForeignKey(Artiste, on_delete=models.CASCADE)
+class Lyric(models.Model):
+    content = models.TextField()
+    song_id = models.ForeignKey(Song, on_delete=models.CASCADE)
